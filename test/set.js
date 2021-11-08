0602cdd6e48b5cd2e23239611b49ed0c363a9f73
Marcus Poehls <marcus@futurestud.io>
2021-11-08 10:46:34 +0100

    bring back accidentally removed code

--- test/set.js
+++ test/set.js
@@ -544,6 +544,16 @@ test('findLastIndex', () => {
   expect(ids.findLastIndex(id => id > 3)).toBe(3)
   expect(ids.findLastIndex(id => id > 10)).toBe(-1)
 
+  const set = Set.from([
+    { id: 1, name: 'Marcus', subscribed: true },
+    { id: 2, name: 'Norman', subscribed: true },
+    { id: 3, name: 'Christian', subscribed: true }
+  ])
+
+  expect(set.findLastIndex((value) => value.subscribed)).toEqual(2)
+  expect(set.findLastIndex((value) => value.name === 'Supercharge')).toBe(-1)
+})
+
 test('intersect', () => {
   const ids = Set.from(1, 2, 3, 4)
 
